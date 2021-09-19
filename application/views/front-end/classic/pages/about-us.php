8de2b43aad545f6dbc2c9b426799b944f5766fbe
Azzaz Khan <students.mails9@gmail.com>
2021-09-19 23:14:31 +0500

    Added english and spanish content for about us page

--- application/views/front-end/classic/pages/about-us.php
+++ application/views/front-end/classic/pages/about-us.php
@@ -15,9 +15,111 @@
     </div>
     <div class="text-justify">
         <div class="hrDiv">
-            <?php
-            var_dump($about_us);
-            ?>
+            <?php if ($this->lang->is_loaded["web_labels_lang.php"] == "spanish") : ?>
+                <div class="about-us-content-wrapper spanish">
+                    <!-- Who we are -->
+                    <div style="margin-bottom: 20px;">
+                        <h2>¿Quiénes somos?</h2>
+                        <p>
+                            Fundamos Depósito Dental "Dentus Supplies" en el año 2021 y se encuentra ubicado en la Ciudad de Tijuana, Baja California, México.
+                        </p>
+                        <p>
+                            Somos una empresa dedica a la adquisición, almacenamiento, distribución y transporte de medicamentos, dispositivos
+                            médicos y demás insumos para la salud.
+                        </p>
+                    </div>
+                    <!-- Mission -->
+                    <div style="margin-bottom: 20px;">
+                        <h2>Misión</h2>
+                        <p>
+                            Somos un deposito dental orientado en la atención de dentistas para la venta y distribución de medicamentos y demás
+                            insumos de uso odontológico, buscando mejorar nuestro servicio de manera continua, ofrecer los productos que necesiten
+                            a tiempo y con los mejores precios del mercado.
+                        </p>
+                    </div>
+                    <!-- Vision -->
+                    <div style="margin-bottom: 20px;">
+                        <h2>Visión</h2>
+                        <p>
+                            Queremos brindar un servicio de excelencia para llegar a ser número uno en distribución de materiales odontológicos y
+                            ayudar a los especialistas en odontología a desarrollar todo su potencial proporcionando los productos necesarios para
+                            brindar el mejor servicio, en la ciudad de Tijuana Baja California y, posteriormente, a nivel nacional.
+                        </p>
+                    </div>
+                    <!-- Our Values -->
+                    <div style="margin-bottom: 20px;">
+                        <h2>Nuestros Valores</h2>
+                        <p>
+                            <strong>Orientación al cliente:</strong> Consideramos al dentista un consumidor activo que demanda una buena comunicación.
+                        </p>
+                        <p>
+                            <strong>Respeto:</strong> Trataremos a nuestros empleados, clientes y proveedores de manera justa y respetuosa.
+                        </p>
+                        <p>
+                            <strong>Confianza:</strong> Pilar fundamental para establecer relaciones duraderas, basadas en el trato personalizado con rigor y
+                            profesionalidad.
+                        </p>
+                        <p>
+                            <strong>Compromiso:</strong> HTener los materiales pedidos en el tiempo establecido.
+                        </p>
+                        <p>
+                            <strong>Liderazgo:</strong> : Interés por el desarrollo personal y profesional de nuestros colaboradores, siendo condición necesaria para el mejoramiento y fortalecimiento de la organización.
+                        </p>
+                        <p>
+                            <strong>Innovación:</strong> Buscaremos continuamente formas de mejorar las operaciones en general y ofreceremos la mejor tecnología.
+                        </p>
+                    </div>
+                </div>
+            <?php else : ?>
+                <div class="about-us-content-wrapper english">
+                    <!-- Who we are -->
+                    <div style="margin-bottom: 20px;">
+                        <h2>Who we are?</h2>
+                        <p>
+                            We founded Depósito Dental "Dentus Supplies" in 2021 and it is located in the City of Tijuana, Baja California, Mexico.
+                        </p>
+                        <p>
+                            We are a company dedicated to the acquisition, storage, distribution and transportation of medicines, medical devices and other health supplies.
+                        </p>
+                    </div>
+                    <!-- Mission -->
+                    <div style="margin-bottom: 20px;">
+                        <h2>Mission</h2>
+                        <p>
+                            We are a dental warehouse focused on the care of dentists for the sale and distribution of medicines and other supplies for dental use, seeking to continuously improve our service, offer the products they need on time and with the best prices in the market.
+                        </p>
+                    </div>
+                    <!-- Vision -->
+                    <div style="margin-bottom: 20px;">
+                        <h2>Vision</h2>
+                        <p>
+                            We are a dental warehouse focused on the care of dentists for the sale and distribution of medicines and other supplies for dental use, seeking to continuously improve our service, offer the products they need on time and with the best prices in the market.
+                        </p>
+                    </div>
+                    <!-- Our Values -->
+                    <div style="margin-bottom: 20px;">
+                        <h2>Our Values</h2>
+                        <p>
+                            <strong>Customer orientation:</strong> We consider the dentist an active consumer who demands good communication.
+                        </p>
+                        <p>
+                            <strong>Respect for people:</strong> We will treat our employees, customers and suppliers fairly and respectfully.
+                        </p>
+                        <p>
+                            <strong>Trust:</strong> Fundamental pillar to establish lasting relationships, based on personalized treatment with rigour and professionalism.
+                        </p>
+                        <p>
+                            <strong>Commitment:</strong> Have the materials ordered in the established time.
+                        </p>
+                        <p>
+                            <strong>Leadership:</strong> Interest in the personal and professional development of our collaborators, is a necessary condition for the improvement and strengthening of the organization.
+                        </p>
+                        <p>
+                            <strong>Innovation:</strong> We will continually seek ways to improve overall operations and offer the best technology
+                        </p>
+                    </div>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 </section>
